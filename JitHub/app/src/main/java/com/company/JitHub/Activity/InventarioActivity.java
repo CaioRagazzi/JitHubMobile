d0cc48992a4a4794fae8d1b5715dc05c9953dfce
Caio Ragazzi <ca.ragazzi@gmail.com>
2019-04-24 02:29:36 -0300

    refactor

--- JitHub/app/src/main/java/com/company/JitHub/Activity/InventarioActivity.java
+++ JitHub/app/src/main/java/com/company/JitHub/Activity/InventarioActivity.java
@@ -1,33 +1,46 @@
 package com.company.JitHub.Activity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.NonNull;
+
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
-import android.view.KeyEvent;
+
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+
+import android.view.ViewParent;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-import android.widget.Spinner;
+
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.company.JitHub.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.Map;
+
 
 public class InventarioActivity extends AppCompatActivity {
 
@@ -36,6 +49,8 @@ public class InventarioActivity extends AppCompatActivity {
     String documentoAtual;
 
     LinearLayout ll = null;
+    TextInputLayout ti = null;
+    LinearLayout lm = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +64,7 @@ public class InventarioActivity extends AppCompatActivity {
     private void ListaNivel1() {
         position = 0;
 
-        final LinearLayout lm = findViewById(R.id.linearMain);
-        lm.removeView(ll);
-        ll.removeAllViews();
+        lm.removeAllViews();
 
         db.collection("Nivel1")
                 .get()
@@ -69,8 +82,6 @@ public class InventarioActivity extends AppCompatActivity {
                                     btn.setOnClickListener(new View.OnClickListener() {
                                         @Override
                                         public void onClick(View v) {
-                                            lm.removeView(ll);
-                                            ll.removeAllViews();
                                             position = 2;
                                             ListaNivel2(document.getId());
                                         }
@@ -79,18 +90,17 @@ public class InventarioActivity extends AppCompatActivity {
                                     btn.setOnClickListener(new View.OnClickListener() {
                                         @Override
                                         public void onClick(View v) {
-                                            ll.removeAllViews();
                                             position = 2;
                                             getPerguntas("Nivel1", document.getId());
                                         }
                                     });
                                 }
 
-                                ll.addView(btn);
+                                lm.addView(btn);
 
                             }
 
-                            lm.addView(ll);
+//                            lm.addView(ll);
                         } else {
                             Log.d("tagerr", "Error getting documents: ", task.getException());
                         }
@@ -101,12 +111,8 @@ public class InventarioActivity extends AppCompatActivity {
 
     private void ListaNivel2(final String documento){
         position = 2;
-
         documentoAtual = documento;
-
-        final LinearLayout lm = findViewById(R.id.linearMain);
-        lm.removeView(ll);
-        ll.removeAllViews();
+        lm.removeAllViews();
 
         db.collection("Nivel1/" + documento + "/Nivel2")
                 .get()
@@ -124,18 +130,17 @@ public class InventarioActivity extends AppCompatActivity {
                                     btn.setOnClickListener(new View.OnClickListener() {
                                         @Override
                                         public void onClick(View v) {
-                                            ll.removeAllViews();
                                             position = 3;
                                             getPerguntas("Nivel1/" + documento + "/Nivel2", document.getId());
                                         }
                                     });
                                 }
 
-                                ll.addView(btn);
+                                lm.addView(btn);
 
                             }
 
-                            lm.addView(ll);
+//                            lm.addView(ll);
                         } else {
                             Log.d("tagerr", "Error getting documents: ", task.getException());
                         }
@@ -145,10 +150,11 @@ public class InventarioActivity extends AppCompatActivity {
 
     private void getPerguntas(String collection,String document){
 
+        lm.removeAllViews();
+
         final LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
 
-        final LinearLayout lm = findViewById(R.id.linearMain);
         lm.removeView(ll);
         ll.removeAllViews();
 
@@ -156,6 +162,7 @@ public class InventarioActivity extends AppCompatActivity {
                 .document(document)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @SuppressLint("ResourceType")
                     @Override
                     public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                         if (task.isSuccessful()) {
@@ -169,29 +176,80 @@ public class InventarioActivity extends AppCompatActivity {
                                 Object nome = d.getKey();
 
                                 if (nome != null) {
-                                    ll.setOrientation(LinearLayout.VERTICAL);
 
-                                    TextView txt = new TextView(InventarioActivity.this);
-                                    txt.setLayoutParams(params);
-                                    txt.setText(nome.toString());
-                                    txt.setVisibility(View.VISIBLE);
-
-                                    EditText editText = new EditText(InventarioActivity.this);
+                                    TextInputEditText editText = new TextInputEditText(InventarioActivity.this);
+                                    editText.setHint(nome.toString());
                                     editText.setLayoutParams(params);
                                     editText.setVisibility(View.VISIBLE);
 
-                                    ll.addView(txt);
-                                    ll.addView(editText);
-
+                                    ti.addView(editText);
+                                    lm.addView(ti);
+                                    ti = new TextInputLayout(InventarioActivity.this);
                                 }
                             }
+
                             Button button = new Button(InventarioActivity.this);
                             button.setLayoutParams(params);
+                            button.setHeight(30);
                             button.setText("Enviar");
                             button.setVisibility(View.VISIBLE);
-                            ll.addView(button);
 
-                            lm.addView(ll);
+                            button.setOnClickListener(new View.OnClickListener() {
+                                @RequiresApi(api = Build.VERSION_CODES.O)
+                                @Override
+                                public void onClick(View v) {
+
+                                    Map<String, Object> resposta = new HashMap<>();
+                                    int childCount = lm.getChildCount();
+                                    for (int i = 0; i < childCount; i++){
+                                        View view = lm.getChildAt(i);
+                                        if (view instanceof TextInputLayout){
+
+                                            int childCount1 = ((TextInputLayout) view).getChildCount();
+                                            for (int o = 0; o<childCount1; o++){
+                                                View view2 = ((TextInputLayout) view).getChildAt(o);
+                                                if (view2 instanceof FrameLayout){
+
+                                                    int childCount2 = ((FrameLayout) view2).getChildCount();
+                                                    for (int p = 0; p<childCount2; p++){
+                                                        View childAt = ((FrameLayout) view2).getChildAt(p);
+                                                        if (childAt instanceof EditText){
+
+                                                            String s1 = ((TextInputLayout) view).getHint().toString();
+                                                            String s = ((EditText) childAt).getText().toString();
+
+                                                            resposta.put(s1, s);
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+
+                                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+                                    LocalDateTime now = LocalDateTime.now();
+
+                                    db.collection("respostas")
+                                            .document(dtf.format(now))
+                                            .set(resposta)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Log.d("Add", "DocumentSnapshot successfully written!");
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Log.w("Err", "Error writing document", e);
+                                                }
+                                            });
+                                }
+                            });
+
+                            lm.addView(button);
+
+//                            lm.addView(ll);
 
                         } else {
                             Log.d("tagerr", "Error getting documents: ", task.getException());
@@ -201,6 +259,7 @@ public class InventarioActivity extends AppCompatActivity {
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onBackPressed() {
 
@@ -209,8 +268,37 @@ public class InventarioActivity extends AppCompatActivity {
             InventarioActivity.this.startActivity(myIntent);
             finish();
         } else if (position == 2) {
+            int childCount = lm.getChildCount();
+            for (int i = 0; i < childCount; i++){
+                View childAt = lm.getChildAt(i);
+//                Log.d("tagerr", childAt.toString());
+                if (childAt instanceof android.support.v7.widget.Toolbar){
+                    continue;
+                } else if (childAt instanceof TextInputLayout) {
+                    ((TextInputLayout) childAt).removeAllViews();
+                }else {
+                    lm.removeView(childAt);
+                }
+            }
+
+//            lm.removeView(ll);
+//            lm.removeView(ti);
+//            ll.removeAllViews();
+//            ti.removeAllViews();
             ListaNivel1();
         } else if (position == 3) {
+            int childCount = lm.getChildCount();
+            for (int i = 0; i < childCount; i++){
+                View childAt = lm.getChildAt(i);
+                if (!(childAt instanceof Toolbar)){
+                    lm.removeView(childAt);
+                }
+            }
+
+//            lm.removeView(ll);
+//            lm.removeView(ti);
+//            ll.removeAllViews();
+//            ti.removeAllViews();
             ListaNivel2(documentoAtual);
         }
     }
@@ -218,5 +306,9 @@ public class InventarioActivity extends AppCompatActivity {
     private void InstanciaLl(){
         ll = new LinearLayout(InventarioActivity.this);
         ll.setOrientation(LinearLayout.VERTICAL);
+
+        ti = new TextInputLayout(InventarioActivity.this);
+
+        lm = findViewById(R.id.linearMain);
     }
 }
